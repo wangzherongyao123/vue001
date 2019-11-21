8b6b290fda93084a0388ff964a44fccf0fce2972
wanghuai <2515323344@qq.com>
2019-11-21 20:19:13 +0800

    test tj

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <div>
+        <h1>你好啊</h1>
+
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path=require('path')
+var htmlWebpackPlugin=require('html-webpack-plugin')
+const VueLoaderPlugin=require('vue-loader/lib/plugin')
+
+module.exports={
+    entry:path.join(__dirname,'./src/main.js'),
+    output: {
+        path:path.join(__dirname,'./dist'),
+        filename: "bundle.js"
+    },
+
+    plugins: [
+
+       new htmlWebpackPlugin({
+           template:path.join(__dirname,'./src/index.html'),
+           filename:'index.html'
+       }),
+        new VueLoaderPlugin()
+
+
+    ],
+    module: {
+        rules: [{
+            test:/\.css$/, use: ['style-loader','css-loader']},{test:/\.vue$/,use:'vue-loader'}
+         ]
+    },
+    resolve: {
+        alias: {
+            'vue$':'vue/dist/vue.js'
+        }
+    }
+
+}
