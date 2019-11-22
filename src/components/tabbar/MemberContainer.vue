ef7f2f685fc456f28965d6faa18107d7c4f219ce
wanghuai <2515323344@qq.com>
2019-11-22 17:32:31 +0800

    2day

---
+++ src/components/tabbar/MemberContainer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+<h1>MemberComponent</h1>
+
+    </div>
+</template>
+<script>
+
+</script>
+<style  scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@ import Vue from 'vue'
 import app from './App.vue'
 import {Header} from 'mint-ui'
 import './css/mui.css'
-
+import './css/icons-extra.css'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 Vue.component(Header.name, Header);
+import router from './router.js'
+import { Swipe, SwipeItem } from 'mint-ui';
 
+import VueRources from 'vue-resource'
+Vue.use(VueRources)
+
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 var vm=new Vue({
 
     el:"#app",
-    render:c=>c(app)
+    render:c=>c(app),
+    router
 
 })
